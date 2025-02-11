32b5522369225037e2307608464069a4745e12d3
danielaj2004 <danielajoya12@gmail.com>
2025-02-11 16:27:29 -0500

    completed task 2

--- cc_example.js
+++ cc_example.js
@@ -4,6 +4,17 @@ let products = [
     {name: "Desk chair", price: 150, category: "furniture" }
 ];
 function getProductByCategory(products, category) {
-    return products.filter(products => product.category === category);
+    return products.filter(products => products.category === category);
 }
 console.log(getProductByCategory(products, "electronics"));
+
+// task 2
+function applyDiscount(products, discountRate) {
+    return products.map(product => ({
+        ...product,
+        price: product.price  - (product.price * discountRate)
+    }
+
+    ))
+};
+console.log(applyDiscount(products, 0.1));
